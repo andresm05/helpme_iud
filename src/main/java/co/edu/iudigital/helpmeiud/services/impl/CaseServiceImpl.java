031cc91f6d874499843c54e474e1de85800bfb85
Jaime Muñoz <jaime.munozq@udea.edu.co>
2023-07-19 17:06:46 -0500

    minor modifications

--- src/main/java/co/edu/iudigital/helpmeiud/services/impl/CaseServiceImpl.java
+++ src/main/java/co/edu/iudigital/helpmeiud/services/impl/CaseServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import co.edu.iudigital.helpmeiud.exceptions.EntityNotFoundException;
-import co.edu.iudigital.helpmeiud.exceptions.NotEnabledUserException;
 import co.edu.iudigital.helpmeiud.exceptions.RestException;
 import co.edu.iudigital.helpmeiud.exceptions.UnauthorizedException;
 import co.edu.iudigital.helpmeiud.models.dto.request.CaseDtoRequest;
